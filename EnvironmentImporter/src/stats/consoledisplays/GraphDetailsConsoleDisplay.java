c5a26524304b378abdc49224ab980d9db75c1f0c
vaisagh <vaisaghvt@gmail.com>
2013-02-20 02:07:13 +0800

    Added stat report, libraries, fixed bugs
    * Fixed some bugs in caching code calculation for Phase set
    * Added stat display for each agent and combination of agents
    * Fixed some bugs in how things were being displayed in the same frame
    if summarizing.
    * Added library files to the project folder so that syncing with the
    repository will be more straightforward the next time.

---
+++ EnvironmentImporter/src/stats/consoledisplays/GraphDetailsConsoleDisplay.java
@@ -0,0 +1,19 @@
+package stats.consoledisplays;
+
+import java.util.HashMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vaisagh
+ * Date: 2/1/13
+ * Time: 1:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GraphDetailsConsoleDisplay extends ConsoleDisplay<HashMap<String, ? extends Number>> {
+
+
+
+    @Override
+    public void display(HashMap<String, ? extends Number> data) {
+    }
+}
---
+++ EnvironmentImporter/src/stats/statisticshandlers/GraphDetailsStatisticHandler.java
@@ -0,0 +1,46 @@
+package stats.statisticshandlers;
+
+import com.google.common.collect.HashMultimap;
+import gui.NetworkModel;
+import gui.Phase;
+import gui.StatsDialog;
+import stats.StatisticChoice;
+import stats.chartdisplays.StaircaseVisitChartDisplay;
+import stats.consoledisplays.StaircaseVisitConsoleDisplay;
+
+import java.util.Collection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vaisagh
+ * Date: 2/1/13
+ * Time: 12:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GraphDetailsStatisticHandler extends StatisticsHandler<StaircaseVisitConsoleDisplay, StaircaseVisitChartDisplay> {
+
+
+
+    public GraphDetailsStatisticHandler() {
+        super(new StaircaseVisitChartDisplay(),
+                new StaircaseVisitConsoleDisplay()
+        );
+
+    }
+
+
+    @Override
+    public void generateAndDisplayStats(Collection<String> dataNames, Phase phase, StatsDialog.AllOrOne all, StatsDialog.AggregationType itemAt) {
+        final StatisticChoice choice = StatisticChoice.STAIRCASE_VISIT_CHANCE;
+        HashMultimap<String, String> usesStaircaseUnusuallyList =  ((NetworkModel) NetworkModel.instance()).getStaircaseRelatedMotion(dataNames, phase);
+        this.chartDisplay.setTitle(choice.toString()+phase.toString());
+        this.chartDisplay.setPhase(phase);
+        this.chartDisplay.display(usesStaircaseUnusuallyList);
+        this.consoleDisplay.display(usesStaircaseUnusuallyList);
+
+    }
+
+
+
+
+}
