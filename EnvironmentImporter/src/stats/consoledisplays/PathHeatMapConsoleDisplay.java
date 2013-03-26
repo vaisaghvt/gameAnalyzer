ebaa1923dc763f2b76ea4bb0f952ffad57c7a04f
Vaisagh <vaisaghvt@gmail.com>
2013-03-26 12:10:51 +0800

    heatmap console display dispalys significant data

--- EnvironmentImporter/src/stats/consoledisplays/PathHeatMapConsoleDisplay.java
+++ EnvironmentImporter/src/stats/consoledisplays/PathHeatMapConsoleDisplay.java
@@ -1,5 +1,9 @@
 package stats.consoledisplays;
 
+import gui.NetworkModel;
+
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 /**
@@ -14,20 +18,54 @@ public class PathHeatMapConsoleDisplay extends ConsoleDisplay<HashMap<String, Ha
 
     @Override
     public void display(HashMap<String, HashMap<String, Double>> data) {
-        HashMap<String, Double> sumOfProbs = new HashMap<String, Double>();
-        for(String source : data.keySet()){
-            System.out.print(source +"\t");
+        HashMap<String, Integer> roomToCodeMapping = generateNewRoomToCodeMapping(data);
+        ArrayList<String> higherInData = new ArrayList<String>();
+        ArrayList<String> lowerInData = new ArrayList<String>();
+        for (String source : roomToCodeMapping.keySet()) {
+            Integer sourceId = roomToCodeMapping.get(source);
 
-            double sum =0;
-            for(String destination: data.get(source).keySet()){
-                System.out.print(
-                        "To "+destination+":"+data.get(source).get(destination)+"\t");
-                sum+= data.get(source).get(destination);
+
+            for (String destination : roomToCodeMapping.keySet()) {
+
+                Integer destinationId = roomToCodeMapping.get(destination);
+                if (!data.containsKey(source) || !data.get(source).containsKey(destination)) {
+
+                } else {
+                    double value =  (data.get(source).get(destination)+1.0)/2;
+                    if(value>0.6){
+                        higherInData.add(source +"to"+ destination);
+                    } else if(value<0.4){
+                        lowerInData.add(source+"to"+destination);
+                    }
+                }
             }
-            sumOfProbs.put(source, sum);
-            System.out.println();
+
+        }
+
+        System.out.println(" ********* HIgher in Data **********");
+        for(String value : higherInData){
+            System.out.println(value);
+        }
+
+        System.out.println(" ********* Lower in Data ***********");
+        for(String value : lowerInData){
+            System.out.println(value);
         }
 
 
+
+    }
+
+    private HashMap<String, Integer> generateNewRoomToCodeMapping(HashMap<String, HashMap<String, Double>> data) {
+        int i = 0;
+        //Graph<ModelObject, ModelEdge> graph = NetworkModel.instance().getCompleteGraph();
+        Collection<String> rooms= NetworkModel.instance().getFloorNeighbourSortedRooms();
+        HashMap<String, Integer> nameToCodeMapping = new HashMap<String, Integer>();
+        for (String name : rooms) {
+//            System.out.println(i+":"+name);
+            nameToCodeMapping.put(name, i++);
+        }
+
+        return nameToCodeMapping;
     }
 }
