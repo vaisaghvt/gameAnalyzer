c6f4c886a8ef0354d5030c691dc37366dbdf4488
Vaisagh VT <vaisaghvt@gmail.com>
2013-04-12 22:33:11 +0800

    Better heat map calculation
    Parallelized parts of heat map calculation
    Used an explicit garbage collector which has to be checked
    Added heatmap dialog box to choose options for the heat map display
    This however is yet to be mapped to the actual heat map being generated

---
+++ EnvironmentImporter/src/stats/statisticshandlers/HeatMapComparisonDialog.java
@@ -0,0 +1,11 @@
+package stats.statisticshandlers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vaisagh
+ * Date: 4/12/13
+ * Time: 7:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HeatMapComparisonDialog {
+}
